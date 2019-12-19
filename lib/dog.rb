98e5f6f462bc437c649a9a8ba74748d878c97eca
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-19 22:03:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  @@all =[]
+
+  def initialize(name)
+    @name=name
+    self.save
+  end
+  def self.save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear_all
+    all.clear
+  end
+  def self.print_all
+    @@all.each{ |e|puts e  }
+  end
+end
